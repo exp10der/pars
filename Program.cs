25dda493160639b6d46c2042f35d9cb179d9f332
Konstantin <alx0000@yandex.ru>
2015-04-09 21:46:39 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using HtmlAgilityPack;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string @base = "http://www.cds.spb.ru";
+            string str = string.Empty;
+            using (var client = new HttpClient())
+            {
+                str = client.GetStringAsync("http://www.cds.spb.ru/novostroiki-peterburga/").Result;
+            }
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(str);
+
+
+           #region tmp
+            //var q = doc.DocumentNode.SelectNodes("//a[@class='b-object ']").Select(n =>
+            //{
+            //    var title_tmp = n.Element("header").Element("h2").InnerHtml.Trim();
+
+            //    return new
+            //    {
+            //        url = @base + n.GetAttributeValue("href", ""),
+            //        title = title_tmp
+            //    };
+            //});
+           #endregion
+
+
+            // С текст сканером
+            // 
+            var result = doc.DocumentNode.SelectNodes("//a[@class='b-object ']").Select(n =>
+            {
+                var title_tmp = n.Element("header").Element("h2").InnerHtml.Trim();
+                var scan = new TextScanner(title_tmp);
+                scan.Skip("&laquo;");
+                return new
+                {
+                    url = @base + n.GetAttributeValue("href", ""),
+                    title = scan.ReadTo("&raquo;") 
+                };
+            });
+
+            var list = new List<string>(result.Select(n=>n.title));
+
+
+        }
+    }
+}
